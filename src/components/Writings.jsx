a53be732e6a4f11404fcc8a40dd45ba7678731a6
Joshua O'Bannon <jtobannon@gmail.com>
2021-02-06 22:40:14 -0700

    Update Writing Component

--- src/components/Writings.jsx
+++ src/components/Writings.jsx
@@ -1,34 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState, useEffect, useCallback, useMemo,
+} from 'react';
 import { useQuery, gql, ApolloConsumer } from '@apollo/client';
 import { Query } from 'react-apollo';
-import Box from '@material-ui/core/Box';
+import {
+  CssBaseline, Link, Grid, Box, CircularProgress, makeStyles,
+} from '@material-ui/core';
 import FeaturedPost from './FeaturedPost';
 
 require('dotenv').config();
 
+const useStyles = makeStyles((theme) => ({
+  linkStyle: {
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+}));
+
 export default function Writings() {
-  const [Entries, setEntries] = useState(undefined);
-  const runQuery = gql`
-		query GetBlogCollection {
-			blogPostCollection {
-				items {
-					title
-					slug
-					description
-					publishDate
-				}
-			}
-		}
-`;
-
-  const accessToken = 'lskcNHk-XOra9XrdSIxgzBprlkN4iN9fH8FFf2tTqvQ';
-  const spaceID = '52xx7e8krtto';
-
+  const classes = useStyles();
   const POSTS_QUERY = gql`
     query GetBlogCollection {
       blogPostCollection {
         items {
           title
+          heroImage {
+            url
+          }
           slug
           description
           publishDate
@@ -37,49 +36,33 @@ export default function Writings() {
     }
   `;
 
-  const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
   const { loading, error, data } = useQuery(POSTS_QUERY);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  console.log(data);
-
-  function getPosts() {
-    // setEntries(data);
-  }
-
-  if (data !== 'undefined') {
-    var collection = data.blogPostCollection.items.map((item) => {
-      <FeaturedPost
-        title={item.title}
-        date={item.publishDate}
-        description={item.description}
-        disabled
-        // image={dryerPhoto}
-        // onClick={withLink('/post', markedPost)}
-      />;
-    });
-    console.log(collection);
-  }
 
   return (
     <>
-      <Box style={{ marginTop: '300px' }}>
-          {collection}
-        <ApolloConsumer>
-
-          {(client) => {
-            // <Query query={getPosts} />;
-          }}
-          {/* {Entries} */}
-        </ApolloConsumer>
+      <CssBaseline />
+      <Box style={{ marginTop: '100px', margin: 'auto' }}>
+        <Grid container direction="column" spacing={3}>
+          {data.blogPostCollection.items.map((item) => (
+            <Grid item style={{ margin: 'auto' }}>
+              <Link
+                to={`/${item.slug}`}
+                href={`/writings/${item.slug}`}
+                target="#"
+                className={classes.linkStyle}
+              >
+                <Box>
+                  <FeaturedPost
+                    post={item}
+                    disabled
+                  />
+                </Box>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </>
   );
